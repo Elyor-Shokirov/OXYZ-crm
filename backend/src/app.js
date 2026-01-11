0b323a50006e1724fc34450d9a40bd346b2d4bd8
Elyor Shokirov <elton08@list.ru>
2026-01-11 18:08:11 +0200

    fix middleweare order

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,12 +7,14 @@ import { sequelize } from './config/database.js'
 const PORT = process.env.PORT || 3000
 const app = express()
 
+// ✅ MIDDLEWARE - routes dan OLDIN bo'lishi kerak!
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //Routes
 import authRouters from './routes/auth.routes.js'
 
-app.use('/auth', authRouters)
+app.use('/auth', authRouters) // ← middleware dan KEYIN
 
 app.get('/', (req, res) => {
 	res.send('Hello World')
@@ -24,10 +26,11 @@ app.get('/', (req, res) => {
 		console.log('✅ Database connected')
 		await sequelize.sync()
 		console.log('✅ Models synced')
-		app.listen(PORT, () => {
+		app.listen(PORT, '0.0.0.0', () => {
 			console.log(`🚀 Server running on http://localhost:${PORT}`)
 		})
 	} catch (error) {
 		console.error('❌ DB error:', error.message)
+		process.exit(1)
 	}
 })()
