f7e90663a2388f368f9c4a2570e452940c438040
Elyor Shokirov <elton08@list.ru>
2026-01-11 20:02:52 +0200

    add middleWare1

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express'
+import { verifyToken } from '../middlewares/auth.middleware.js'
+
 const router = Router()
 const { createUser, getAllUsers, getUser } = await import(
 	'../controller/auth.controller.js'
